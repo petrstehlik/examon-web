83ec3d248c0570a00f81524800fa9b78ade57fda
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-13 16:32:43 +0200

    Configurable address to the server

--- api/liberouterapi/modules/kairos/base.py
+++ api/liberouterapi/modules/kairos/base.py
@@ -4,9 +4,10 @@ from pyKairosDB import connect, metadata, reader
 import json
 import requests
 
-conn = connect(server="130.186.13.80", port=8010,
-        user=config["kairosdb"].get("user"),
-        passw=config["kairosdb"].get("password"))
+conn = connect(server = config["kairosdb"].get("server"),
+        port = config["kairosdb"].get("port"),
+        user = config["kairosdb"].get("user"),
+        passw = config["kairosdb"].get("password"))
 
 def generate_health_url():
     return("{0}://{1}:{2}/api/v1/health".format(conn.schema, conn.server, conn.port))
