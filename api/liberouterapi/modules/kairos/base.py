6cfe2f4ae9bbd52d74f07f7d78ce68952ce02e07
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-24 16:52:57 +0200

    Various experiments with KairosDB

--- api/liberouterapi/modules/kairos/base.py
+++ api/liberouterapi/modules/kairos/base.py
@@ -10,17 +10,26 @@ conn = connect(server = config["kairosdb"].get("server"),
         passw = config["kairosdb"].get("password"))
 
 base_query = {
-            "start_absolute" : 1500554800.000,
-            "end_absolute" : 1500555005,
+            "start_absolute" :  1500554895,
+            "end_absolute" :    1500554900,
             "metrics" : [
                     {
                         "tags" : {
                             "org" : ["cineca"],
                             "cluster" : ["galileo"],
-                            "node" : ["node062"],
-                            #"core" : ["0", "1"]
+                            "node" : ["node061", "node062"],
+                            # "core" : ["0", "1"]
+                        }
+                    },
+                    {
+                        "tags" : {
+                            "org" : ["cineca"],
+                            "cluster" : ["galileo"],
+                            "node" : ["node061", "node062"],
+                            # "core" : ["0", "1"]
                         }
                     }
+
                 ]
         }
 
@@ -42,7 +51,7 @@ fra = {
       "group_by": [
         {
             "name": "tag",
-           "tags": [
+            "tags": [
               "core"
             ]
         }
@@ -55,6 +64,9 @@ fra = {
     "unit": "minutes"
   }
 }
+
+def generate_base_url():
+    return("{0}://{1}:{2}/api/v1".format(conn.schema, conn.server, conn.port))
 
 def generate_health_url():
     return("{0}://{1}:{2}/api/v1/health".format(conn.schema, conn.server, conn.port))
@@ -69,15 +81,47 @@ def status():
 	res = requests.get(generate_health_url() + "/status", auth=(conn.user, conn.passw))
 	return(str(res.content))
 
+@app.route("/kairos/metrics")
+def list_metrics():
+    """
+    List all available metrics
+    """
+    res = requests.get(generate_base_url() + "/metricnames", auth=(conn.user, conn.passw))
+    print(res.content)
+    return(str(res.content))
+
+@app.route("/kairos/tags")
+def list_tags():
+    """
+    List all available metrics
+    """
+    res = requests.get(generate_base_url() + "/tagnames", auth=(conn.user, conn.passw))
+    print(res.content)
+    return(str(res.content))
+
+
 def group(query):
     query["metrics"][0]["group_by"] = [
         {
             "name": "tag",
             "tags": [
-                "core"
+                "core", "node"
             ]
         }]
     print(query)
+    return query
+
+def agg_and_group(query):
+    #query = group(query)
+    query["metrics"][0]["aggregators"] = [
+            {
+                "name" : "avg",
+                #"align_sampling" : True,
+                "sampling" : {
+                        "value" :(base_query["end_absolute"] - base_query["start_absolute"]),
+                        "unit" : "minutes"
+                    }
+            }]
     return query
 
 @app.route("/kairos/basic")
@@ -94,3 +138,18 @@ def basic():
             )
 
     return(json.dumps(res))
+
+@app.route("/kairos/sum")
+def sum_base():
+    res= reader.read(conn,
+            ["temp"],
+            start_absolute = base_query["start_absolute"],
+            end_absolute = base_query["end_absolute"],
+            tags = base_query["metrics"][0]["tags"],
+            query_modifying_function = agg_and_group
+            #tags = {"node" : ["node061"]}
+            #only_read_tags = True
+            )
+
+    return(json.dumps(res))
+
