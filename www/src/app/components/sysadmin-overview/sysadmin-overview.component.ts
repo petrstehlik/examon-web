4fe690c884203c306282104b61399d556de80b19
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-26 13:58:25 +0200

    Sysadmin dashboard overview layout

---
+++ www/src/app/components/sysadmin-overview/sysadmin-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ex-sysadmin-overview',
+  templateUrl: './sysadmin-overview.component.html',
+  styleUrls: ['./sysadmin-overview.component.scss']
+})
+export class SysadminOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
