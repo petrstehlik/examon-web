b7bda47af8440fe174a5cae7ad39ecc7eb45ce6a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-26 13:56:47 +0200

    Public dashboard component

---
+++ www/src/app/components/public-dashboard/public-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ex-public-dashboard',
+  templateUrl: './public-dashboard.component.html',
+  styleUrls: ['./public-dashboard.component.scss']
+})
+export class PublicDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
