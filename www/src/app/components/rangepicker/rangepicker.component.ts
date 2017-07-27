ba6a3e36418ac369b722f01ab90f2c363549e5bc
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-27 16:54:23 +0200

    Working rangepicker. Woot! Woot!
    Similar to grafana, it just works.

--- www/src/app/components/rangepicker/rangepicker.component.ts
+++ www/src/app/components/rangepicker/rangepicker.component.ts
@@ -1,15 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { NgbDateStruct, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'ex-rangepicker',
-  templateUrl: './rangepicker.component.html',
-  styleUrls: ['./rangepicker.component.scss']
+    selector: 'ex-rangepicker',
+    templateUrl: './rangepicker.component.html',
+    styleUrls: ['./rangepicker.component.scss']
 })
 export class RangepickerComponent implements OnInit {
 
-  constructor() { }
+    @Output("onSelect")
+    select : EventEmitter<Object> = new EventEmitter<Object>();
 
-  ngOnInit() {
-  }
+    @ViewChild("dropdown") dropdown;
 
+    public intervals_minutes : Array<number> = [1, 2, 5, 10, 15, 30, 45];
+    public intervals_hours : Array<number> = [1, 2, 3, 6, 12, 24, 48];
+
+    from_date: NgbDateStruct;
+    from_time: NgbTimeStruct;
+    to_date: NgbDateStruct;
+    to_time: NgbTimeStruct;
+    query : Object = {
+        to: null,
+        from: null
+    };
+
+    constructor() { }
+
+    ngOnInit() {
+        this.reset();
+    }
+
+    /**
+      * Manually adjust date in query since we are using it as a composite
+      * for both the date and time
+      */
+    selectDate(event, type : "to" | "from") {
+        let prev = new Date(this.query[type]);
+
+        if (event['hour'] && event['minute']) {
+            prev.setMinutes(event['minute']);
+            prev.setHours(event['hour']);
+        } else {
+            prev.setFullYear(event['year']);
+            prev.setMonth(event['month']);
+            prev.setDate(event['day']);
+        }
+
+        this.query[type] = prev.getTime();
+
+        console.log(this.query)
+    }
+
+    /**
+     * Select a relative interval for query
+     */
+    selectInterval(mins : number) {
+        this.query = {
+            from : +Date.now() - mins*60000,
+            to : +Date.now()
+        }
+
+        this.apply();
+    }
+
+    apply() {
+        this.select.emit(this.query);
+        this.dropdown.close();
+    }
+
+    private reset(params = {}) {
+        const from_time = new Date();
+        const to_time = new Date();
+
+        this.query = {
+            from : +Date.now() - 60000,
+            to : +Date.now()
+        }
+
+        this.from_date = {
+            year: from_time.getFullYear(),
+            month: from_time.getMonth(),
+            day: from_time.getDate()
+        }
+        this.from_time = {
+            hour : (from_time.getHours() - 1),
+            minute : from_time.getMinutes(),
+            second : 0
+        }
+
+        this.to_date = {
+            year: to_time.getFullYear(),
+            month: to_time.getMonth(),
+            day: to_time.getDate()
+        }
+        this.to_time = {
+            hour : to_time.getHours(),
+            minute : to_time.getMinutes(),
+            second : 0
+        }
+    }
 }
