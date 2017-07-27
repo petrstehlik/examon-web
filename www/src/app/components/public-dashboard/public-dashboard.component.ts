49623f51bf2a80e4250e3a367d52138169f996d1
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-27 16:56:55 +0200

    Add base for the first dashboard data

--- www/src/app/components/public-dashboard/public-dashboard.component.ts
+++ www/src/app/components/public-dashboard/public-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'ex-public-dashboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicDashboardComponent implements OnInit {
 
-  constructor() { }
+    data : Object = {};
 
-  ngOnInit() {
-  }
+    constructor(private http : HttpClient) { }
+
+    ngOnInit() {
+    }
+
+    public onSelect(time : Object) : void {
+        this.http.get('/api/jobs/stats/total', {
+            params: new HttpParams()
+                        .set('from', time['from'])
+                        .set('to', time['to'])
+        }).subscribe(data => {
+            this.data["total"] = data;
+        })
+    }
 
 }
--- www/src/app/components/public-overview/public-overview.component.ts
+++ www/src/app/components/public-overview/public-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ex-public-overview',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./public-overview.component.scss']
 })
 export class PublicOverviewComponent implements OnInit {
+
+    @Input("data")
+    set setData(data) {
+        console.log(data);
+    };
 
   constructor() { }
 
