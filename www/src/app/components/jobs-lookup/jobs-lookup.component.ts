17c245506f2b4a8832bf4134f551d45ed0d423d2
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-28 15:01:43 +0200

    Add job interface

--- www/src/app/components/jobs-lookup/jobs-lookup.component.ts
+++ www/src/app/components/jobs-lookup/jobs-lookup.component.ts
@@ -3,6 +3,34 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MessageService } from 'app/services';
 
+interface Job {
+    qtime : number;
+    start_time : number;
+    end_time : number;
+    backup_qtime : number;
+    ctime : number;
+    mean_power : any;
+    var_list : string;
+    qlist : string;
+    account_name : string;
+    project : string;
+    user_id : string;
+    job_id : string;
+    job_name : string;
+    queue : string;
+    nmics_req : number;
+    ngpus_req : number;
+    ncpus_req : number;
+    nnodess_req : number;
+    mem_req : number;
+    req_time : number;
+    mpiprocs : number;
+    node_list : string;
+    vnode_list : string;
+    used_nodes : string;
+    used_cores : string;
+}
+
 @Component({
   selector: 'app-jobs-lookup',
   templateUrl: './jobs-lookup.component.html',
@@ -11,14 +39,14 @@ import { MessageService } from 'app/services';
 export class JobsLookupComponent implements OnInit {
 
     public jobid : string;
-    public lastjob : Object;
+    public lastjob : Job;
 
     constructor(private router : Router,
         private http : HttpClient,
         private msg : MessageService) { }
 
     ngOnInit() {
-        this.http.get('/api/jobs/latest').subscribe(
+        this.http.get<Job>('/api/jobs/latest').subscribe(
             data => {
                 this.lastjob = data;
             },
