4b25a7aab31599e9bf5cab65640a921adec72236
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-28 13:55:17 +0200

    First public dashboard with a chart and agg data

--- www/src/app/components/public-overview/public-overview.component.ts
+++ www/src/app/components/public-overview/public-overview.component.ts
@@ -1,20 +1,64 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
-  selector: 'ex-public-overview',
-  templateUrl: './public-overview.component.html',
-  styleUrls: ['./public-overview.component.scss']
+    selector: 'ex-public-overview',
+    templateUrl: './public-overview.component.html',
+    styleUrls: ['./public-overview.component.scss']
 })
 export class PublicOverviewComponent implements OnInit {
 
-    @Input("data")
-    set setData(data) {
-        console.log(data);
+    private time : Object;
+    public data : Object = {
+        total : null
+    }
+
+    public chart_data = {};
+
+    @Input('time')
+    set setData(time) {
+        console.log(time);
+        if (time != undefined) {
+            this.time = time;
+            this.fetchTotal();
+            this.fetchClusterLoad();
+        }
     };
 
-  constructor() { }
+    constructor(private http : HttpClient) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() { }
+
+    private fetchTotal() {
+        this.http.get('/api/jobs/stats/total', {
+            params: new HttpParams()
+                        .set('from', this.time['from'])
+                        .set('to', this.time['to'])
+        }).subscribe(data => {
+            this.data["total"] = data;
+        })
+    }
+
+    private fetchClusterLoad() {
+        this.http.get('/api/kairos/load', {
+            params : new HttpParams()
+                        .set('from', this.time['from'])
+                        .set('to', this.time['to'])
+        }).subscribe(data => {
+            let tmp_data = [];
+
+            for(let key of Object.keys(data["points"])) {
+                tmp_data.push([new Date(+key * 1000), ...data["points"][key]])
+            }
+
+            let tmp = {
+                "labels" : ["Date", ...data["labels"]],
+                "data" : tmp_data
+            }
+
+            this.chart_data["cluster_load"]  = tmp;
+        });
+
+    }
 
 }
