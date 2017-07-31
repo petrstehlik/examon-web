909bbf1d5a83b7f16bdd16703584fc510f02dd08
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-07-31 14:56:58 +0200

    Parametrized queries for per-node and per-cluster data

--- api/liberouterapi/modules/kairos/base.py
+++ api/liberouterapi/modules/kairos/base.py
@@ -1,7 +1,9 @@
 from liberouterapi import config, app
+from .error import JobsError
+from .utils import check_times, generate_health_url, generate_base_url, extract_data
+from .Aggregate import Aggregate
 
 from flask import request
-
 from pyKairosDB import connect, metadata, reader
 import json
 import requests
@@ -10,25 +12,6 @@ conn = connect(server = config["kairosdb"].get("server"),
         port = config["kairosdb"].get("port"),
         user = config["kairosdb"].get("user"),
         passw = config["kairosdb"].get("password"))
-
-base_query = {
-    "start_absolute" :  1500554860,
-    "end_absolute" :    1500554900,
-    "metrics" : [
-            {
-                "tags" : {
-                    "org" : ["cineca"],
-                    "cluster" : ["galileo"]
-                }
-            }
-        ]
-    }
-
-def generate_base_url():
-    return("{0}://{1}:{2}/api/v1".format(conn.schema, conn.server, conn.port))
-
-def generate_health_url():
-    return("{0}://{1}:{2}/api/v1/health".format(conn.schema, conn.server, conn.port))
 
 @app.route("/kairos/health")
 def health():
@@ -58,105 +41,40 @@ def list_tags():
     print(res.content)
     return(str(res.content))
 
-def group(query):
-    return group_tags(query, ["core", "node"])
-
-def group_tags(query, tags):
-    query["metrics"][0]["group_by"] = [
-        {
-            "name": "tag",
-            "tags": tags
-        }]
-    return query
-
-def group_nodes(query):
-    return group_tags(query, ["node"])
-
-def agg_and_group(query):
-    #query = group(query)
-    query["metrics"][0]["aggregators"] = [
-            {
-                "name" : "avg",
-                #"align_sampling" : True,
-                "sampling" : {
-                        "value" :(base_query["end_absolute"] - base_query["start_absolute"]),
-                        "unit" : "minutes"
-                    }
-            }]
-    return query
-
-def aggregate_5_secs(query):
-    #query = group(query)
-    q2 = group_tags(query, ["node"])
-    q2["metrics"][0]["aggregators"] = [
-            {
-                "name" : "avg",
-                "align_sampling" : True,
-                "align_start_time" : True,
-                "sampling" : {
-                        "value" : 10,
-                        "unit" : "seconds"
-                    }
-            }]
-    return q2
-
-
-@app.route("/kairos/basic")
-def basic():
-    res= reader.read(conn,
-            ["temp"],
-            start_absolute = base_query["start_absolute"],
-            #start_relative = (5, "minutes"),
-            end_absolute = base_query["end_absolute"],
-            tags = base_query["metrics"][0]["tags"],
-            query_modifying_function = group
-            #tags = {"node" : ["node061"]}
-            #only_read_tags = True
-            )
-
-    return(json.dumps(res))
-
-@app.route("/kairos/sum")
-def sum_base():
-    res= reader.read(conn,
-            ["temp"],
-            start_absolute = base_query["start_absolute"],
-            end_absolute = base_query["end_absolute"],
-            tags = base_query["metrics"][0]["tags"],
-            query_modifying_function = agg_and_group
-            #tags = {"node" : ["node061"]}
-            #only_read_tags = True
-            )
-
-    return(json.dumps(res))
-
-
-@app.route("/kairos/load")
+@app.route("/kairos/node")
 def load_base():
+    """
+    Load data on per-node level in dygraphs-friendly format
+
+    Params:
+        from    <required>  timestamp
+        to                  timestamp
+        metric  <required>  metric by which to query
+        raw                 return KairosDB data format (no post-processing)
+        aggregate   <def:5> size of time window (s) in which to aggregate data (and alignment of timestamps)
+    """
     args = request.args.to_dict()
 
-    if "from" in args:
-        # Convert to int so we can compare it
-        args["from"] = int(args["from"])
-    else:
-        raise JobsError("Missing 'from' in GET parameters")
+    check_times(args)
 
-    if "to" not in args:
-        # Generate timestamp
-        args["to"] = int(time.time()) * 1000
-    else:
-        args["to"] = int(args["to"])
+    agg = Aggregate(5)
+    agg.set_tags(['node'])
 
-    if args["to"] < args["from"]:
-        raise JobsError("'from' time cannot precede 'to' time")
+    if "metric" not in args:
+        raise JobsError("Missing 'metric' in GET parameters")
 
+    if "aggregate" in args:
+        agg.set_window(int(args["aggregate"]))
 
-    res= reader.read(conn,
-            ["load_core"],
+    res = reader.read(conn,
+            [args["metric"]],
             start_absolute = args["from"] / 1000,
             end_absolute = args["to"] / 1000,
-            tags = base_query["metrics"][0]["tags"],
-            query_modifying_function = aggregate_5_secs
+            tags = {
+                "org" : ["cineca"],
+                "cluster" : ["galileo"]
+            },
+            query_modifying_function = agg.aggregate
             )
 
     if "raw" in args:
@@ -165,15 +83,59 @@ def load_base():
     labels = list()
     data = dict()
 
-    for result in res["queries"][0]["results"]:
-        labels.append(result["group_by"][0]["group"]["node"])
-        for item in result["values"]:
-            round_time = int(round(item[0], -1))
+    extract_data(res, data, labels, "node")
 
-            if str(round_time) not in data:
-                data[str(round_time)] = [item[1]]
-            else:
-                data[str(round_time)].append(item[1])
+    return(json.dumps({
+        "points" : data,
+        "labels" : labels,
+        "metric" : res["queries"][0]["results"][0]["name"]
+    }))
+
+@app.route("/kairos/cluster")
+def cluster_level():
+    """
+    Load data on per-cluster level in dygraphs-friendly format
+
+    Params:
+        from    <required>  timestamp
+        to                  timestamp
+        metric  <required>  metric by which to query
+        raw                 return KairosDB data format (no post-processing)
+        aggregate   <def:5> size of time window (s) in which to aggregate data (and alignment of timestamps)
+    """
+    args = request.args.to_dict()
+
+    check_times(args)
+
+    agg = Aggregate(5)
+    agg.set_tags(['cluster'])
+
+    if "metric" not in args:
+        raise JobsError("Missing 'metric' in GET parameters")
+
+    metric = args["metric"]
+
+    if "aggregate" in args:
+        agg.set_window(int(args["aggregate"]))
+
+    res = reader.read(conn,
+            [args["metric"]],
+            start_absolute = args["from"] / 1000,
+            end_absolute = args["to"] / 1000,
+            tags = {
+                "org" : ["cineca"],
+                "cluster" : ["galileo"]
+            },
+            query_modifying_function = agg.aggregate
+            )
+
+    if "raw" in args:
+        return(json.dumps(res))
+
+    labels = list()
+    data = dict()
+
+    extract_data(res, data, labels, "cluster")
 
     return(json.dumps({
         "points" : data,
