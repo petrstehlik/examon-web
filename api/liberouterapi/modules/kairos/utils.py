4ac671e4555316f212bbd4457513b88894c35375
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-01 16:52:34 +0200

    Join multiple metrics data to single chart (preferably stacked)

--- api/liberouterapi/modules/kairos/utils.py
+++ api/liberouterapi/modules/kairos/utils.py
@@ -1,4 +1,5 @@
 from .error import JobsError
+import time
 
 def check_times(args):
     """
@@ -25,10 +26,18 @@ def generate_base_url():
 def generate_health_url():
     return("{0}://{1}:{2}/api/v1/health".format(conn.schema, conn.server, conn.port))
 
+def merge_dicts(x, y):
+    """Given two dicts, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
 
 def extract_data(raw_data, data, labels, grouper):
     for result in raw_data["queries"][0]["results"]:
-        labels.append(result["group_by"][0]["group"][grouper])
+        label_name = ''
+        for name in grouper:
+            label_name += '/' + result["group_by"][0]["group"][name]
+        labels.append(label_name)
 
         for item in result["values"]:
             round_time = int(round(item[0], 1))
@@ -37,3 +46,26 @@ def extract_data(raw_data, data, labels, grouper):
                 data[str(round_time)] = [item[1]]
             else:
                 data[str(round_time)].append(item[1])
+
+def join_data(data):
+    labels = list()
+    metrics = list()
+    points = dict()
+
+    for dataset in data:
+        for item in dataset["labels"]:
+            labels.append(item + '/' + dataset["metric"])
+
+        for item in dataset["points"]:
+            if item in points:
+                points[item] += dataset["points"][item]
+            else:
+                points[item] = dataset["points"][item]
+
+        metrics.append(dataset["metric"])
+
+    return({
+            "points" : points,
+            "labels" : labels,
+            "metric" : metrics
+        })
