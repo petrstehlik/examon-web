c3389d387534ae0a9e32cbf9b67a25ce905f81f1
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-01 16:51:37 +0200

    Devise new gaps aggregator, fix error and rename add_tags

--- api/liberouterapi/modules/kairos/Aggregate.py
+++ api/liberouterapi/modules/kairos/Aggregate.py
@@ -12,24 +12,38 @@ class Aggregate():
         if window != self.default_window:
             self.window = window
 
-    def set_tags(self, tags):
+    def set_group_tags(self, tags):
         self.tags = tags
 
-    def reset():
+    def reset(self):
         self.window = self.default_window
 
+    def attach_agg(self, query):
+        """
+        Attach aggregator field to a query if not present
+        """
+
+        for item in query["metrics"]:
+            if "aggregators" not in item:
+                item["aggregators"] = list()
+
+        return query
+
     def group_tags(self, query):
-        query["metrics"][0]["group_by"] = [
-            {
-                "name": "tag",
-                "tags": self.tags
-            }]
+        for item in query["metrics"]:
+            item["group_by"] = [
+                {
+                    "name": "tag",
+                    "tags": self.tags
+                }]
         return query
 
     def aggregate(self, query):
-        q2 = self.group_tags(query)
-        q2["metrics"][0]["aggregators"] = [
-                {
+        query = self.attach_agg(query)
+        query = self.group_tags(query)
+
+        for item in query["metrics"]:
+            item["aggregators"].append({
                     "name" : "avg",
                     "align_sampling" : True,
                     "align_start_time" : True,
@@ -37,6 +51,22 @@ class Aggregate():
                             "value" : self.window,
                             "unit" : "seconds"
                         }
-                }]
+                })
+        return query
 
+    def gaps(self, query):
+        query = self.attach_agg(query)
+        q2 = self.group_tags(query)
+        q2 = self.aggregate(q2)
+
+        for item in q2["metrics"]:
+            item["aggregators"].append({
+                    "name" : "gaps",
+                    #"align_sampling" : True,
+                    "align_start_time" : True,
+                    "sampling" : {
+                        "value" : self.window,
+                        "unit" : "seconds"
+                    }
+                })
         return q2
