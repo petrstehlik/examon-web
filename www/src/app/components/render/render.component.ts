976454951ee167731e3dfda13101ab12c49f9bd8
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-04 14:36:06 +0200

    Add link to SO solution for getting offset

--- www/src/app/components/render/render.component.ts
+++ www/src/app/components/render/render.component.ts
@@ -145,16 +145,17 @@ export class RenderComponent implements OnInit, OnDestroy {
             }
         }
 
-            function getOffset( el ) {
-                var _x = 0;
-                var _y = 0;
-                while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-                            _x += el.offsetLeft - el.scrollLeft;
-                            _y += el.offsetTop - el.scrollTop;
-                            el = el.offsetParent;
-                        }
-                return { top: _y, left: _x };
-            }
+        // https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+        function getOffset( el ) {
+            var _x = 0;
+            var _y = 0;
+            while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                        _x += el.offsetLeft - el.scrollLeft;
+                        _y += el.offsetTop - el.scrollTop;
+                        el = el.offsetParent;
+                    }
+            return { top: _y, left: _x };
+        }
 
 
         });
