d3dd6e5531ae9b777c3c1248ec07dc10d2a96524
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-09 12:43:53 +0200

    Add all IPMI data to selector

--- www/src/app/components/render/render.component.ts
+++ www/src/app/components/render/render.component.ts
@@ -20,21 +20,62 @@ export class RenderComponent implements OnInit, OnDestroy {
             value : "Ambient_Temp"
         },
         {
-            name : "Package Temperature",
-            value : "temp_pkg"
+            name : "PIB Ambient Temperature",
+            value : "PIB_Ambient_Temp"
         },
         {
             name : "Power Consumption",
-            value : "pow_pkg"
+            value : "Avg_Power"
         },
         {
             name : "CPU Load",
-            value : "load_core"
+            value : "CPU_Utilization"
         },
         {
-            name : "IPS",
-            value : "ips"
-        }]
+            name : "System Load",
+            value : "Sys_Utilization"
+        },
+        {
+            name : "Memory Load",
+            value : "Mem_Utilization"
+        },
+        {
+            name : "IO Load",
+            value : "IO_Utilization"
+        },
+        {
+            name : "Chipset Temperature",
+            value : "PCH_Temp"
+        },
+        {
+            name : "HDD Inlet Temperature",
+            value : "HDD_Inlet_Temp"
+        },
+        {
+            name : "PCI Riser 1 Temperature",
+            value : "PCI_Riser_1_Temp"
+        },
+        {
+            name : "PCI Riser 2 Temperature",
+            value : "PCI_Riser_2_Temp"
+        },
+        {
+            name : "GPU Outlet Temperature",
+            value : "GPU_Outlet_Temp"
+        },
+        {
+            name : "CPU #1 Temperature",
+            value : "CPU1_Temp"
+        },
+        {
+            name : "CPU #2 Temperature",
+            value : "CPU2_Temp"
+        },
+        {
+            name : "CPU Temperature",
+            value : "temp_pkg"
+        },
+]
 
     constructor() { }
 
