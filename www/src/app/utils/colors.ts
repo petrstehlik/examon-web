34aae15085507e2618367fe6efa2be9cf4dc4e23
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-10 16:37:39 +0200

    Extract getOffset function

--- www/src/app/utils/colors.ts
+++ www/src/app/utils/colors.ts
@@ -22,3 +22,16 @@ export function calcOffset(value : number, min : number, max : number) {
     return((value - min)/(max - min) * 100);
 }
 
+
+// https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+export function getOffset(el) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+                _x += el.offsetLeft - el.scrollLeft;
+                _y += el.offsetTop - el.scrollTop;
+                el = el.offsetParent;
+            }
+    return { top: _y, left: _x };
+}
+
