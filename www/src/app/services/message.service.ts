15ed7bb9926575575fe35723430c5118d76f8a66
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-16 16:35:04 +0200

    WWW: Catch error WS messages and display them

--- www/src/app/services/message.service.ts
+++ www/src/app/services/message.service.ts
@@ -2,11 +2,29 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs/Subject';
 
+import { environment as env } from 'environments/environment';
+
+declare const io;
+
 @Injectable()
 export class MessageService {
     private subject = new Subject<any>();
 
-    constructor() { }
+    private socket;
+
+    constructor() {
+        this.socket = io.connect(env.ws.host + ':' + env.ws.port + '/jobs');
+        this.socket = io.connect(env.ws.host + ':' + env.ws.port + '/render');
+
+        this.socket.on('connect', () => {
+            console.info('Message service connected');
+        });
+
+        this.socket.on('error', (data) => {
+            console.error("ERROR", data);
+            this.subject.next({ text: data, type : 'danger' });
+        })
+    }
 
     send(message: string, type: 'warning'|'danger'|'info'|'success' = 'warning') {
         this.subject.next({ text: message, type : type });
