a33036bdd9b4b89135b50a89161db0383e784144
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2018-03-04 16:38:29 +0100

    Force debug mode

--- api/__main__.py
+++ api/__main__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import logging
 
-logging.basicConfig()
+logging.basicConfig(level=logging.DEBUG)
 
 from muapi import socketio, config, app
 
 if __name__ == '__main__':
+    app.config.from_object(config)
     socketio.run(app, debug=True, port=5555)
             #port = app.config["PORT"],
             #host = app.config["HOST"])
